ab2b41fc699d43778a251353b317823d0be15de4
norraynor <37270508+Norraynor@users.noreply.github.com>
2024-04-01 20:02:08 +0200

    Setup files and webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+			title: "Restaurant Page",
+		}),
+	],
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+};
